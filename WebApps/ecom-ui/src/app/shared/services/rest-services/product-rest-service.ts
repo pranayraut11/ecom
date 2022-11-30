b613dac99fd8a30ca55243eb256927fd3740c190
pranayraut11 <p.raut@serrala.com>
2022-12-01 01:33:59 +0530

    Catalog url updated

--- WebApps/ecom-ui/src/app/shared/services/rest-services/product-rest-service.ts
+++ WebApps/ecom-ui/src/app/shared/services/rest-services/product-rest-service.ts
@@ -10,11 +10,11 @@ export class ProductRestService{
     constructor(private rest : HttpClient ){}
     
     getProductList(): Observable<Product[]>{
-      return  this.rest.get<Product[]>("http://catalog/product",{responseType: 'json'});
+      return  this.rest.get<Product[]>("http://catalog-deployment/product",{responseType: 'json'});
     }
 
     createProduct(product : Product): Observable<Product>{
-      return this.rest.post<Product>("http://catalog/product",product);
+      return this.rest.post<Product>("http://catalog-deployment/product",product);
     }
 
 }
