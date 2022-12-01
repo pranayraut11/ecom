c507710ac702d07affa0ddc76defaea74fcc2aac
pranayraut11 <p.raut@serrala.com>
2022-12-01 13:12:04 +0530

    Catalog service type changed to Cluster Ip

--- WebApps/ecom-ui/src/app/shared/services/rest-services/product-rest-service.ts
+++ WebApps/ecom-ui/src/app/shared/services/rest-services/product-rest-service.ts
@@ -1,7 +1,6 @@
 import { Injectable } from "@angular/core";
 import { HttpClient } from "@angular/common/http";
 import { Product } from "src/app/shared/models/product.model";
-import { map } from "rxjs/operators";
 import { Observable } from "rxjs";
 
 @Injectable({"providedIn":"root"})
@@ -10,11 +9,11 @@ export class ProductRestService{
     constructor(private rest : HttpClient ){}
     
     getProductList(): Observable<Product[]>{
-      return  this.rest.get<Product[]>("http://catalog-deployment/product",{responseType: 'json'});
+      return  this.rest.get<Product[]>("http://catalog-deployment:8080/product",{responseType: 'json'});
     }
 
     createProduct(product : Product): Observable<Product>{
-      return this.rest.post<Product>("http://catalog-deployment/product",product);
+      return this.rest.post<Product>("http://catalog-deployment:8080/product",product);
     }
 
 }
