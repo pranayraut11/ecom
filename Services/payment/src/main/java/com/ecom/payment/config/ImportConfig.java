f620b1fde3a8f6503a58a2f89ccd414cbc8f7425
pranayraut11 <p.raut@serrala.com>
2023-05-14 10:01:16 +0530

    Common package updated

--- Services/payment/src/main/java/com/ecom/payment/config/ImportConfig.java
+++ Services/payment/src/main/java/com/ecom/payment/config/ImportConfig.java
@@ -1,6 +1,6 @@
 package com.ecom.payment.config;
 
-import com.ecom.shared.config.common.ImportCommonClasses;
+import com.ecom.shared.common.config.ImportCommonClasses;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
 
