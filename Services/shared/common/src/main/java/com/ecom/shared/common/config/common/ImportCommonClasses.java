0f529997cc931e7bbc31234975f11f8be1164090
pranayraut11 <p.raut@serrala.com>
2023-07-21 17:02:17 +0530

    Security app/started excluded

--- Services/shared/common/src/main/java/com/ecom/shared/common/config/common/ImportCommonClasses.java
+++ Services/shared/common/src/main/java/com/ecom/shared/common/config/common/ImportCommonClasses.java
@@ -6,12 +6,13 @@ import com.ecom.shared.common.config.i18.Translator;
 import com.ecom.shared.common.config.rest.WebClientConfig;
 import com.ecom.shared.common.config.security.KeycloakConfiguration;
 import com.ecom.shared.common.config.security.SecurityConfig;
+import com.ecom.shared.common.controller.AppController;
 import com.ecom.shared.common.exception.EcomExceptionHandler;
 import com.ecom.shared.common.validation.DtoValidator;
 import org.springframework.context.annotation.Import;
 import org.springframework.context.annotation.PropertySource;
 
-@Import(value = {DtoValidator.class, SecurityConfig.class, Translator.class, KeycloakConfiguration.class, EcomExceptionHandler.class, WebClientConfig.class, I18Config.class})
+@Import(value = {DtoValidator.class, SecurityConfig.class, Translator.class, KeycloakConfiguration.class, EcomExceptionHandler.class, WebClientConfig.class, I18Config.class, AppController.class})
 @PropertySource("classpath:common.properties")
 public class ImportCommonClasses {
 }
