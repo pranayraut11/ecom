abe390219ba4a4c7fb99ccd0047a909e97befeed
pranayraut11 <p.raut@serrala.com>
2023-07-21 19:58:45 +0530

    API added for starter prob

--- Services/shared/common/src/main/java/com/ecom/shared/common/controller/AppController.java
+++ Services/shared/common/src/main/java/com/ecom/shared/common/controller/AppController.java
@@ -2,9 +2,11 @@ package com.ecom.shared.common.controller;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController("app")
+@RestController()
+@RequestMapping("app")
 public class AppController {
     @GetMapping("started")
     public ResponseEntity<Void> health(){
