125a11161cd3880fcd5adca40cb31b3229658b08
P.Raut <p.raut@serrala.com>
2023-10-04 20:39:13 +0530

    Http connection pooling added

--- Services/shared/common/src/main/java/com/ecom/shared/common/config/rest/WebClientConfig.java
+++ Services/shared/common/src/main/java/com/ecom/shared/common/config/rest/WebClientConfig.java
@@ -4,12 +4,17 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpHeaders;
+import org.springframework.http.client.reactive.ReactorClientHttpConnector;
 import org.springframework.util.StringUtils;
 import org.springframework.web.context.request.RequestContextHolder;
 import org.springframework.web.context.request.ServletRequestAttributes;
 import org.springframework.web.reactive.function.client.ClientRequest;
 import org.springframework.web.reactive.function.client.ExchangeFilterFunction;
 import org.springframework.web.reactive.function.client.WebClient;
+import reactor.netty.http.client.HttpClient;
+import reactor.netty.resources.ConnectionProvider;
+
+import java.time.Duration;
 
 @Slf4j
 @Configuration
@@ -17,7 +22,14 @@ public class WebClientConfig {
 
     @Bean
     public WebClient webClient(WebClient.Builder builder) {
-        return builder.filter(logFilter()).build();
+        String connectionProviderName = "myConnectionProvider";
+        log.info("Creating http connection...");
+        int maxConnections = 100;
+        int acquireTimeout = 1000;
+        HttpClient httpClient = HttpClient.create(ConnectionProvider.create(connectionProviderName,maxConnections));
+        builder = WebClient.builder().clientConnector( new ReactorClientHttpConnector(httpClient));
+        log.info("Http connection created with max connection {}", httpClient.configuration().connectionProvider().maxConnections());
+        return builder.filter(logFilter()). build();
     }
 
 
