eec017911718f63bf2ba6c34b03efe3f6e47834d
P.Raut <p.raut@serrala.com>
2023-12-16 15:39:10 +0530

    UI changes

--- WebApps/ecom-ui/src/app/features/profile/components/address/list-address/list-address.component.ts
+++ WebApps/ecom-ui/src/app/features/profile/components/address/list-address/list-address.component.ts
@@ -38,6 +38,6 @@ export class ListAddressComponent implements OnInit {
   }
 
   updateAddress(id : string){
-    this.route.navigate(['user/profile/address/create/'+id]);
+    this.route.navigate(['user/profile/address/create/']);
   }
 }
